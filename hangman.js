f6e83b6111f5b5ad26d5a5ac54b798a07d5611e3
justin deguzman <justindeguzman@justins-mbp.attlocal.net>
2017-10-19 18:19:35 -0700

    firatPush

---
+++ hangman.js
@@ -0,0 +1,37 @@
+function Word() = {
+
+}
+
+function Letter() = {
+
+}
+
+
+// word - specific logic and data
+// each letter object 
+// display requirements: 
+
+// an underlying character 
+// blank placeholder (such as an underscore)
+
+
+/* requirements: 
+an Array (of letters)
+an 
+
+
+
+
+
+
+/* words:
+
+// Television
+// SpongeBob
+// Plankton
+// Water
+// Rosebud
+// Boating 
+
+	
+*/
